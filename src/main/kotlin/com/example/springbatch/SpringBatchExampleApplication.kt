f10569f826e33599e97960e8938bad972cb51d2e
Chanqun Sung <cksgns93@naver.com>
2021-08-15 14:47:36 +0900

    first batch

--- src/main/kotlin/com/example/springbatch/SpringBatchExampleApplication.kt
+++ src/main/kotlin/com/example/springbatch/SpringBatchExampleApplication.kt
@@ -1,9 +1,11 @@
 package com.example.springbatch
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableBatchProcessing
 class SpringBatchExampleApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/example/springbatch/part1/HelloConfiguration.kt
@@ -0,0 +1,38 @@
+package com.example.springbatch.part1
+
+import mu.KotlinLogging
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+private val logger = KotlinLogging.logger {}
+
+@Configuration
+class HelloConfiguration(
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val stepBuilderFactory: StepBuilderFactory
+) {
+
+    // --spring.batch.job.names=helloJob 을 주면 이것만 실행
+    @Bean
+    fun helloJob(): Job { //spring batch 의 실행 단위
+        return jobBuilderFactory.get("helloJob")
+            .incrementer(RunIdIncrementer()) // job이 실행할 때 마다 parameter id를 생성
+            .start(this.helloStep()) // job 실행시 최초로 실행 될 메소드
+            .build()
+    }
+
+    @Bean
+    fun helloStep(): Step { //step은 job의 실행 단위 하나의 job은 한 개이상의 step 을 가질 수 있음
+        return stepBuilderFactory.get("helloStep")
+            .tasklet { stepContribution, chunkContext ->
+                logger.info { "hello spring batch" }
+                RepeatStatus.FINISHED
+            }.build()
+    }
+}
